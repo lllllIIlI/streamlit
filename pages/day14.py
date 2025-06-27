26b588b340f34a960c69ccc2f4542329d9266986
cmj <konoha09@naver.com>
2025-06-27 14:32:04 +0900

    .

--- pages/day14.py
+++ pages/day14.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-pr = df.profile_report()
+pr = ProfileReport(df, title="Penguins Data Profiling Report", explorative=True)
 st_profile_report(pr)
